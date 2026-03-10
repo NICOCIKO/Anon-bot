6af8b49f6d1427866a6ed554545f7b31f13ca650
NICOCIKO <98383333+NICOCIKO@users.noreply.github.com>
2026-03-10 21:59:26 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 import os
 
-# Берём токен бота из переменной окружения BOT_TOKEN
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-# Берём ID админа из переменной окружения ADMIN_ID и преобразуем в int
-ADMIN_ID = int(os.getenv("ADMIN_ID"))
+ADMINS = [
+    7924774037,
+    2037648258
+]
